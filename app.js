6ae2236ee97a3e5fed0512a9cd60eb6f4a204bb2
NadishUmair <nadishumair403@gmail.com>
2025-01-11 17:49:08 +0500

    save

--- app.js
+++ app.js
@@ -11,10 +11,20 @@ app.use(express.json());
 
 // CORS settings (adjust allowed origins as needed)
 const allowedOrigins = ['http://localhost:3000', 'http://192.168.56.1:3000'];
+
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS')); 
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+  credentials: true,  
 }));
+
 
 // Static file serving for uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
--- config/db.js
+++ config/db.js
@@ -4,15 +4,15 @@ const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet
 
 const ConnectDb = () => {
   mongoose.connect(mongo_url, {
-    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
-    socketTimeoutMS: 45000, // Socket timeout after 45 seconds
+    serverSelectionTimeoutMS: 5000, 
+    socketTimeoutMS: 45000,
   })
   .then(() => {
     console.log("Database connected successfully");
   })
   .catch((error) => {
     console.error("Error in connecting to the database:", error.message);
-    process.exit(1); // Terminate the process if the connection fails
+    process.exit(1); 
   });
 };
 
