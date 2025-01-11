b4bf0fb39141676c389e1be6b569b27699f18b3d
playnest <playnest.official@gmail.com>
2025-01-11 13:29:09 +0500

    save changes

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ app.use(express.json());
 
 
 
+// const allowedOrigins = ['http://localhost:3000', 'http://192.168.56.1:3000', 'https://playnestfrontend.vercel.app'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS')); 
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],  
+//   credentials: true,  
+// }));
+
+
+
+const allowedOrigins = ['http://localhost:3000', 'http://192.168.56.1:3000'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const ProductRoutes=require("./routes/productsRoutes");
