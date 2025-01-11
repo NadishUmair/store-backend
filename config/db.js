66c6335ba370aa4eda5fc105f12ede273caabfbc
NadishUmair <nadishumair403@gmail.com>
2025-01-11 17:43:33 +0500

    save

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet.mongodb.net/store"; 
+const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet.mongodb.net/store";
 
 const ConnectDb = () => {
   mongoose.connect(mongo_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+    socketTimeoutMS: 45000, // Socket timeout after 45 seconds
   })
   .then(() => {
     console.log("Database connected successfully");
