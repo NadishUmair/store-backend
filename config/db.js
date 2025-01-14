12aae8292f71c939be7e0245f6029bc4171da956
NadishUmair <nadishumair403@gmail.com>
2025-01-14 11:57:06 +0500

    save

--- config/db.js
+++ config/db.js
@@ -6,16 +6,19 @@ const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet
 
 const ConnectDb = () => {
   mongoose.connect(mongo_url, {
-    serverSelectionTimeoutMS: 5000, 
-    socketTimeoutMS: 45000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Database connected successfully");
+  
+
   })
   .catch((error) => {
     console.error("Error in connecting to the database:", error.message);
     process.exit(1); 
   });
 };
+
 
 module.exports = ConnectDb;
