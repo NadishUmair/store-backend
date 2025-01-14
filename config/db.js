d841ee50d978a59ef791e4a67a4d368b9e260c98
NadishUmair <nadishumair403@gmail.com>
2025-01-14 13:22:27 +0500

    save

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 // const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet.mongodb.net/store";
-// const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet.mongodb.net/?retryWrites=true&w=majority&appName=store";
-const mongo_url = "mongodb+srv://nadish:nadish123@cluster0.ebv6jwb.mongodb.net/store";
+const mongo_url = "mongodb+srv://shopcirclestore:nD4lMeKCvlwGO0rS@cluster0.h1iet.mongodb.net/?retryWrites=true&w=majority&appName=store";
+// const mongo_url = "mongodb+srv://nadish:nadish123@cluster0.ebv6jwb.mongodb.net/store";
 
 
 const ConnectDb = () => {
