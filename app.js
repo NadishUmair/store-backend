a9e9140b17ce347759c9dbaf23fe9a5ccea2fbed
NadishUmair <nadishumair403@gmail.com>
2025-01-23 11:11:15 +0500

    save

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ const ConnectDb = require("./config/db");
 const path = require("path");
 const PORT = 4000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 ConnectDb();
 // CORS settings (adjust allowed origins as needed)
-const allowedOrigins = ['http://localhost:3000', 'http://192.168.56.1:3000','https://strore-admin-nine.vercel.app','https://onlinestore-mtcr.vercel.app'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS')); 
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],  
-}));
+// const allowedOrigins = ['http://localhost:3000', 'http://192.168.56.1:3000','https://strore-admin-nine.vercel.app','https://onlinestore-mtcr.vercel.app'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS')); 
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],  
+// }));
 
 
 
